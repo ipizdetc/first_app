50f57d8eec795f0e22c7410cec3d225d411239f3
Ipizdetc <ipizdetc@gmail.com>
2015-01-29 13:58:29 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '0da462a6f8c04654184d300e6e671e56d0bef852175fabbbf1203024e0374393f5a7cb0f661660ccbd25e82e45f14c3f56e7a7b17349adbede4a9e8021bcfb6b'
